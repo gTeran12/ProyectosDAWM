1820df5f5fdd41456c5059c1b3ded76ba1947fd1
gTeran12 <gteran@espol.edu.ec>
2023-01-25 11:16:48 -0500

    Agregada tabla1

--- Proyecto03/clienteAngular/src/app/app-routing.module.ts
+++ Proyecto03/clienteAngular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "about-us", component: AboutUsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Proyecto03/clienteAngular/src/app/app.module.ts
+++ Proyecto03/clienteAngular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
